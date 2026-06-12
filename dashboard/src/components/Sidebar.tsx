94f4ac05c9893e991d14c1b8e43d1fef5e0e6547
Mike Wagner <mike@metify.io>
2026-06-12 13:21:57 +0000

    chore: bump app version to v3.5.9

--- dashboard/src/components/Sidebar.tsx
+++ dashboard/src/components/Sidebar.tsx
@@ -146,7 +146,7 @@ export function Sidebar() {
 
       <div className="p-4 border-t border-gray-800 text-xs text-gray-500 flex justify-between">
         <span>Data refreshes every 5 min</span>
-        <span>v2.8.4</span>
+        <span>v3.5.9</span>
       </div>
     </aside>
   );
